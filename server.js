b23ff97f5fb02ef5bb2f225906037d065117785d
raveena33 <113931058+raveena33@users.noreply.github.com>
2025-02-16 06:44:22 -0500

    updated

--- server.js
+++ server.js
@@ -152,4 +152,4 @@ app.get("/confirm-call", (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
